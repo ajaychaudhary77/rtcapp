b89a1d5c6877e0f86da4e5d3b8da117e44619573
development <development>
2016-05-17 12:19:38 +0200

    updates upto 11/05/2016

--- www/js/CallController.js
+++ www/js/CallController.js
@@ -27,9 +27,8 @@
 		    $scope.call_modal = modal;
 		});
 
-
 		function call(isInitiator, peer_id){
-			
+			alert('calling '+peer_id + ' and isInitiator '+isInitiator)
 			var config = { 
 				isInitiator: isInitiator,
 				stun: {
@@ -42,32 +41,44 @@
 				},
 				streams: {
 				  audio: true,
-				  video: false
+				  video: true
 				}
 			};
+			var phonertc = cordova.require('com.dooble.phonertc.PhoneRTC');
+			try{
+				//session = new cordova.plugins.phonertc.Session(config);
+				var session = new phonertc.Session(config);
 
-			var session = new cordova.plugins.phonertc.Session(config);
-
-			session.on('sendMessage', function(data){ 
-				
-				SocketService.emit('sendMessage', {
-				  'id': id,
-				  'peer_id': $scope.peer_id, 
-				  'type': 'phonertc_handshake',
-				  'data': JSON.stringify(data)
+				phonertc.setVideoView({
+				    container: document.getElementById('video'),
+				    local: {
+				        position: [0, 0],
+				        size: [100, 100]
+				    }
 				});
-			});
+					session.on('sendMessage', function(data){ 
+					
+					SocketService.emit('sendMessage', {
+					  'id': id,
+					  'peer_id': $scope.peer_id, 
+					  'type': 'phonertc_handshake',
+					  'data': JSON.stringify(data)
+					});
+				});
 
-			session.on('disconnect', function(){
-			  SocketService.emit('sendMessage', { 'id': id, 'peer_id': $scope.peer_id, 'type': 'ignore' });
-			  $scope.call_modal.hide();
-			});
+				session.on('disconnect', function(){
+				  SocketService.emit('sendMessage', { 'id': id, 'peer_id': $scope.peer_id, 'type': 'ignore' });
+				  $scope.call_modal.hide();
+				});
 
-			session.call();
+				session.call();
 
-			$scope.contact = session; 
+				$scope.contact = session; 
+			}
+			catch(err){
+				alert(err);
+			}
 		}
-
 
 
 
@@ -91,7 +102,7 @@
 
 
 		$scope.ignore = function(){
-
+			alert("ignore called");
 		  if(JSON.stringify($scope.contact) === '{}'){
 			$scope.contact.disconnect();
 		  }else{
@@ -103,19 +114,19 @@
 
 
 	    $scope.end = function(){
-
+	    	/*alert("Call end called");
 	      $scope.contact.close();
 	      $scope.contact = {};
 
 	      SocketService.emit('sendMessage', { 'id': id, 'peer_id': $scope.peer_id, 'type': 'end' });
 	      $scope.callInProgress = false;
 	      $scope.callEnded = true;
-	      $scope.call_modal.hide();
+	      $scope.call_modal.hide();*/
 	    };
 
 
 	    $scope.answer = function(){
-	    	
+	    	//alert($scope.callInProgress);
 	      if($scope.callInProgress){ 
 	      	return; 
 	      }
@@ -123,13 +134,16 @@
 	      $scope.callInProgress = true;
 
 	      call(false, $scope.peer_id); 
-
+		  setTimeout($scope.updateVideoPosition, 1000);
 	      setTimeout(function(){
 	        SocketService.emit('sendMessage', { 'id': id, 'peer_id': $scope.peer_id, 'type': 'answer' });
 	      }, 1500);
 	    };
 
 
+	    $scope.updateVideoPosition = function () {
+	      $rootScope.$broadcast('videoView.updatePosition');
+	    };
 	    function onMessageReceive(message){
 	
 	      switch(message.type){
